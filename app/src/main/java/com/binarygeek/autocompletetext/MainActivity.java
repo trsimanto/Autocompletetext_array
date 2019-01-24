247f80537b363a631d50785159506241ef045ebb
Towhid <towhidurrahmansimanto@gmail.com>
2019-01-25 05:32:43 +0600

    raw

---
+++ app/src/main/java/com/binarygeek/autocompletetext/MainActivity.java
@@ -0,0 +1,26 @@
+package com.binarygeek.autocompletetext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.countries_list);
+        textView.setAdapter(adapter);
+    }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+}
+
